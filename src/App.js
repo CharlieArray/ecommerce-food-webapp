2a793b68953c7773d3e3bfa6d242e8a0d4968875
Charles Ybarra <charles.ybarra@outlook.com>
2021-07-13 14:06:17 -0500

    webpage template

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
 import './App.css';
 
+//header
+  //div
+    //nav
+      //link - about me
+      //link portfolio projects
+      //link - instagram food api
+      //link - coaching lessons
+    //nav
+  //div
+
+
+//main (use React router to different pages)
+  //about me page
+  //portfolio project page
+  //instagram api page (default MainPage)
+  //coaching lessons page
+//main
+
+
+//footer
+  //div
+    //top of page
+    //contact info
+    //possible contact form
+  //div
+
+
 function App() {
   return (
     <div className="App">
